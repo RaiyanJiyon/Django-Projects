08daa4c6d04a558171d3fa3c7c56ac0acd869ac6
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-06-07 20:01:41 +0600

    Completing The Detail View

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Item
 from django.template import loader
+from .models import Item
 
 # Create your views here.
 def index(request):
@@ -16,4 +16,8 @@ def item(request):
     return HttpResponse('<h1>This is an item view</h1>')
 
 def detail(request, item_id):
-    return HttpResponse("This is item no %s" % item_id)
+    item = Item.objects.get(pk=item_id)
+    context = {
+        'item': item,
+    }
+    return render(request, 'food/detail.html', context=context)
