4b4b78275d5c21132f407645b02db6a1ea2889f7
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-06-08 21:19:31 +0600

    Implementing The Edit Functionality

--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     #food/item
     path('item/', views.item, name='item'),
     # add item
-    path('add/', views.create_item, name='create_item')
+    path('add/', views.create_item, name='create_item'),
+    # update item
+    path('update/<int:id>/', views.update_item, name='update_item')
 ]
