46a0d7947bab00f5a90c821d4d731684cd95ad81
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-06-08 22:10:16 +0600

    Implementing Delete Functionality

--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -12,5 +12,7 @@ urlpatterns = [
     # add item
     path('add/', views.create_item, name='create_item'),
     # update item
-    path('update/<int:id>/', views.update_item, name='update_item')
+    path('update/<int:id>/', views.update_item, name='update_item'),
+    # delete item
+    path('delete/<int:id>/', views.delete_item, name='delete_item')
 ]
