36c8e5720ea435ea9df186dbfc196eed2999426d
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-06-09 20:17:54 +0600

    Creating User Registration Form

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # install food apps
     'food.apps.FoodConfig',
+    # install users apps
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
